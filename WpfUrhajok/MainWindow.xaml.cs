1fdacef264ab84cdda72a4be5057be16726a70c8
qgede <qgede@WINDOWS-8CGEU7Q>
2025-12-10 08:06:31 +0100

    Szalonna József - Adatok betöltése

--- WpfUrhajok/MainWindow.xaml.cs
+++ WpfUrhajok/MainWindow.xaml.cs
@@ -41,5 +41,27 @@ namespace WpfUrhajok
         {
             InitializeComponent();
         }
+
+        List<Urhajo> urhajok = new List<Urhajo>();
+
+        private void betoltes(object sender, RoutedEventArgs e)
+        {
+            urhajok = new List<Urhajo>()
+            {
+                new Urhajo("Falcon-9", 7, 550000, 87, "SpaceX", "Sikeres", 2020),
+                new Urhajo("Orion-X1", 4, 820000, 73, "NASA", "Folyamatban", 2023),
+                new Urhajo("StarRider", 12, 910000, 66, "Boeing", "Sikeres", 2019),
+                new Urhajo("SolarWind", 3, 300000, 92, "ESA", "Sikertelen", 2018),
+                new Urhajo("Galaxy-Cruiser", 18, 1200000, 54, "Blue Origin", "Sikeres", 2022),
+                new Urhajo("Nova-Prime", 10, 760000, 80, "Roscosmos", "Sikeres", 2017),
+                new Urhajo("Pioneer-VII", 6, 640000, 70, "NASA", "Folyamatban", 2024),
+                new Urhajo("Titan-Hawk", 14, 1050000, 48, "SpaceX", "Sikeres", 2021),
+                new Urhajo("CosmoJet", 5, 430000, 93, "JAXA", "Sikertelen", 2016),
+                new Urhajo("Nebula-FX", 11, 870000, 60, "ESA", "Sikeres", 2020)
+            };
+
+            dataGrid.ItemsSource = urhajok;
+        }
+
     }
 }
